19eef7f9c343118924c5609b54d3a4f95b519e08
lqv19a2 <lqvu.25174600078@sv.uneti.edu.vn>
2026-04-28 23:12:34 +0700

    nop bai lab 8

---
+++ 25174600078_LEQUANGVU/bai5.py
@@ -0,0 +1,14 @@
+def in_ascii():
+    while True:
+        ch = input("Nhập ký tự (gõ 'esc' để thoát): ")
+        
+        if ch.lower() == 'esc':   
+            print("Kết thúc chương trình")
+            break
+        
+        if len(ch) == 0:
+            continue
+        
+        print("ASCII của", ch[0], "là:", ord(ch[0]))
+
+in_ascii()
